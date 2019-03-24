b5f99df01d4389b24e0d1b50bc0cbf4c1b2b502a
Hallie Reynolds <hine113@gmail.com>
2019-03-24 09:12:53 -0500

    Update forumdb.py

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,22 @@
 # "Database code" for the DB Forum.
 
-import datetime
+import  psycopg2
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
 
-def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+def get_posts():	
+	"""Return all posts from the 'database', most recent first."""
+	db=	psycopg2.connect("dbname=forum")
+	c=	db.cursor()
+	c.execute("select content,time from posts order by time desc")
+	db.close()
+	return c.fetchall()
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  	"""Add a post to the 'database' with the current timestamp."""
+	db=	psycopg2.connect("dbname=forum")
+	c=	db.cursor()  	
+	c.execute("insert into posts values ('%s')" % content)
+	db.commit()
+	db.close()
 
 
